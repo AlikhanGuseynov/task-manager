034eeb96b31b56c9e212806838be693f0aec7733
Alixan <alikhan-gusejjnv@list.ru>
2021-12-16 15:27:49 +0400

    add validation for login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user: User | undefined;
+
+  constructor(
+    private authService: AuthService
+  ) {
+    this.authService.getCurrentUser().subscribe(event => {
+      this.user = event;
+    })
+  }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/modules/authentication/login/login.component.ts
+++ src/app/modules/authentication/login/login.component.ts
@@ -15,6 +15,8 @@ export class LoginComponent implements OnInit {
     email: '',
     password: ''
   };
+  emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+  formIsValid = true;
 
   constructor(
     private authService: AuthService,
@@ -26,6 +28,8 @@ export class LoginComponent implements OnInit {
   }
 
   login(form: NgForm) {
+    console.log(form.form.valid)
+    this.formIsValid = form.form.valid;
     const loginForm: Login = {
       email: form.form.controls.email.value,
       password: form.form.controls.password.value,
