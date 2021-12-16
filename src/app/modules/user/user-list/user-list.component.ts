a74945586ce5a57f2072013003d8e52f6568d5c2
Alixan <alikhan-gusejjnv@list.ru>
2021-12-16 23:39:17 +0400

    some changes user - list

--- src/app/modules/user/user-list/user-list.component.ts
+++ src/app/modules/user/user-list/user-list.component.ts
@@ -12,6 +12,7 @@ import {DatePipe} from "@angular/common";
 export class UserListComponent implements OnInit {
 
   userList: User[] | undefined;
+  userListCopy: User[] | undefined;
 
   constructor(
     private userService: UserService,
@@ -26,6 +27,7 @@ export class UserListComponent implements OnInit {
   getUserList() {
     this.userService.getUsers().subscribe(event => {
       this.userList = event;
+      this.userListCopy = event;
     })
   }
 
@@ -46,7 +48,32 @@ export class UserListComponent implements OnInit {
   getDate(lastLogin: number | undefined) {
     return this.datePipe.transform(lastLogin, 'dd-MM-yyyy')
   }
+
   getTime(lastLogin: number | undefined) {
     return this.datePipe.transform(lastLogin, 'HH:mm:ss')
   }
+
+  filter(filterInput: HTMLInputElement) {
+    this.userList = this.userListCopy?.filter(item => {
+      return this.filterCheck(item, filterInput.value);
+    })
+  }
+
+  filterCheck(user: User, filterValue: string): boolean {
+    let checkingParams = '';
+    Object.keys(user).map(key => {
+      if (
+        key === 'mobile' || key === 'address' || key === 'userName'
+        || key === 'surname' || key === 'email' || key === 'position' || key === 'department'
+      ) {
+        checkingParams = checkingParams + ' ' + user[key]
+      } else if (key === 'lastLogin') {
+        checkingParams = checkingParams + ' ' + this.datePipe.transform(user[key], 'dd-MM-yyyy HH:mm:ss')
+      }
+    })
+    console.log(checkingParams)
+    return JSON.stringify(checkingParams).toLowerCase().includes(filterValue.toLowerCase());
+  }
+
+
 }
